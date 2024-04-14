2db96c4465300b41bbc61be086d81bd4d3fa0f69
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-04-14 13:20:53 -0700

    update prefect lab

--- flows/healthcheck.py
+++ flows/healthcheck.py
@@ -1,8 +1,8 @@
 import platform
-import prefect
-from prefect import task, flow, get_run_logger
-from prefect.orion.api.server import ORION_API_VERSION
 import sys
+
+import prefect
+from prefect import flow, get_run_logger, task
 
 
 @task
@@ -14,7 +14,6 @@ def log_platform_info():
     logger.info("Platform information (instance type) = %s ", platform.platform())
     logger.info("OS/Arch = %s/%s", sys.platform, platform.machine())
     logger.info("Prefect Version = %s 🚀", prefect.__version__)
-    logger.info("Prefect API Version = %s", ORION_API_VERSION)
 
 
 @flow
